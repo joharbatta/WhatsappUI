b4bd3de2a6201ba84150020b711808dd2c283ae8
joharbatta <joharbatta@gmail.com>
2019-09-07 18:48:08 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'whatsapp_home.dart';
+void main()=>runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "Whatsapp",
+      theme: new ThemeData(
+        primaryColor: new Color(0xff075E54),
+        accentColor: new Color(0xff25D366),
+      ),
+      home: new WhatsAppHome(),
+    );
+  }
+}
---
+++ lib/whatsapp_home.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+class WhatsAppHome  extends StatefulWidget {
+  @override
+  _WhatsAppHomeState createState() => _WhatsAppHomeState();
+}
+
+class _WhatsAppHomeState extends State<WhatsAppHome>
+    with SingleTickerProviderStateMixin {
+
+  TabController _tabController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+     super.initState();
+    _tabController=new TabController(vsync: this,initialIndex: 1,length: 4);
+
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("WhatsApp"),
+        elevation: 0.7,
+        bottom: new TabBar(
+          controller: _tabController,
+          indicatorColor: Colors.white,
+          tabs: <Widget>[
+            new Tab(icon: new Icon(Icons.camera_alt)),
+            new Tab(text: "CHATS"),
+            new Tab(text: "STATUS "),
+            new Tab(text: "CALLS"),
+          ],
+
+        ),
+      ),
+      body: new TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          new CameraScreen(),
+          new ChatScreen(),
+          new StatusScreen(),
+          new CallsScreen(),
+        ],
+      ),
+      floatingActionButton: new FloatingActionButton(
+        backgroundColor: Theme.of(context).accentColor,
+        child: new Icon(Icons.message),
+        onPressed: ()=>print("Open Chats"),
+      ),
+    );
+  }
+}
